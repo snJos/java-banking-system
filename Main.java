0af8d7046989014b77f28f8c4d6eb6ab7b16b9a3
pi3t4 <tablun45@gmail.com>
2024-10-27 21:54:39 +0300

    updated

--- Main.java
+++ Main.java
@@ -1,4 +1,44 @@
 package Banking.com;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        
+        BankAccount account = new BankAccount(name);
+        
+        while (true) {
+            System.out.println("\n--- Bank System Menu ---");
+            System.out.println("1. Deposit");
+            System.out.println("2. Withdraw");
+            System.out.println("3. Check Balance");
+            System.out.println("4. Exit");
+            System.out.print("Choose one: ");
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    Deposit deposit = new Deposit(account);
+                    deposit.execute();
+                    break;
+                case 2:
+                    Withdraw withdraw = new Withdraw(account);
+                    withdraw.execute();
+                    break;
+                case 3:
+                    System.out.println("Current balance: $" + account.getBalance());
+                    break;
+                case 4:
+                    System.out.println("Thank you for using. Goodbye!");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Invalid input.");
+            }
+        }
+    }
 }
+
--- Withdraw.java
+++ Withdraw.java
@@ -1,4 +1,18 @@
 package Banking.com;
 
+import java.util.Scanner;
+
 public class Withdraw {
+    private BankAccount account;
+
+    public Withdraw(BankAccount account) {
+        this.account = account;
+    }
+
+    public void execute() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter amount to withdraw: ");
+        double withdrawAmount = scanner.nextDouble();
+        account.withdraw(withdrawAmount);
+    }
 }
